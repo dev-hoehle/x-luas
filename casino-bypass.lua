b27ef435a9d5b5dbf0d3ae563e02032ded120c2a
Andreas <anditv21@icloud.com>
2023-02-09 21:55:48 +0100

    ✅ Added casino chip bypass

---
+++ casino-bypass.lua
@@ -0,0 +1,55 @@
+g_lua.register()
+
+notify = g_gui.add_toast
+submenu = g_gui.add_submenu
+button = g_gui.add_button
+log = g_logger.log_info
+toggle = g_gui.add_toggle
+
+function STAT_SET_INT(hash, val)
+    STATS.STAT_SET_INT(g_util.joaat("MPPLY_" .. hash), val, true)
+end
+
+function reset()
+    STAT_SET_INT("CASINO_CHIPS_PUR_GD", 0) ---[[chips the player has purchased]]---
+    STAT_SET_INT("CASINO_CHIPS_PUR_GD", 0) ---[[chips the player has won by betting]]---
+end
+
+function welcome()
+    notify("[Casino Bypass Lua] Recovery->Casino Bypass")
+    notify("[Casino Bypass Lua] Welcome to Casino Bypass by anditv21.")
+    notify("[Casino Bypass Lua] Scuccessfully loaded.") 
+
+    log("[Casino Bypass Lua] Successfully loaded.")
+end
+
+welcome()
+submenu("Lua Options", "Casino Bypass")
+submenu("Recovery", "Casino Bypass")
+
+toggle("Casino Bypass", "Bypass purchase limit", false, "Removes the time restriction for purchasing casino chips.", function(on)
+    loop = on
+    while loop do
+		SYSTEM.WAIT(1000)
+        reset()
+	end
+end)
+
+button(
+    "Casino Bypass",
+    "Unload Casino Bypass Lua",
+    function()
+        unload = true
+    end
+)
+
+while true do
+    if unload then
+        notify("[Casino Bypass Lua] Unloading...")
+        log("[Casino Bypass Lua] Unloading...")
+        SYSTEM.WAIT(3000)
+        g_lua.unregister()
+        unload = false
+    end
+    g_util.yield()
+end
